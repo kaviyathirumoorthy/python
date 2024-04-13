2ff0251d4b47fad24a0744f118cb1faf272e661d
Kaviya. T <90082734+kaviyathirumoorthy@users.noreply.github.com>
2024-04-13 14:28:46 +0000

    intrio

---
+++ basics/Introduction.py
@@ -0,0 +1,50 @@
+
+
+#identifier-starts with alphabet or underscore, digits should not in starting but used other than first position, not use other special charcter
+#          -it is case sensitive so Greeeting different from greeting variable.
+
+#variable- reserved memory location to store value
+#        - this means that when you create a variable you reserve some space in memory
+#        - based on datatype memory is allocated.
+
+#Assigning values to variables- python variable do not have explicitly declared to reserve memory space.
+# the declaration happens automatically when you assign a value to a variable
+#the equal sign(=) is used to assign values to variables.
+
+#To get input from user
+''''print("enter name:")
+name=input()
+print("enter age:")
+age=int(input())
+#or
+#name=input("enter name: ")
+#age=int(input("enter age: "))'''
+
+''''print(type(age))
+print(id(age))'''
+5
+
+
+#datatypes
+#The simple data types in python are: Numbers, String, Boolean, complex(3+4j), float
+#datatype conversion
+'''1.Explicit data conversion- Users convert the data type of an object to required data type
+    example: x=32
+    print(str(x))
+   2.Implicit data conversion-takes place automatically during run time
+    example: print(9//5 *27) --float division output:1.8'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
